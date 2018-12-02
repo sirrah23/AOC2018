5cc5f933feab3701920ca40760b3e0934c037c98
sirrah23 <hseth93@gmail.com>
2018-12-02 00:08:32 -0500

    Solution for day 01 is complete

---
+++ 01/01_code.clj
@@ -0,0 +1,34 @@
+(defn getLines 
+  [fname]
+  (with-open [rdr (clojure.java.io/reader fname)]
+      (doall (line-seq rdr))))
+
+(defn toInt 
+  [freqStr]
+  (let [
+    sign (get freqStr 0)
+    num (read-string (subs freqStr 1))]
+  (if (= \+ sign) num (* -1 num))))
+ 
+(defn sum 
+  [nums] 
+  (reduce + nums))
+
+(defn firstFreqTwice 
+  [freqs]
+  (loop [currFreq 0 [currNewFreq & restNewFreqs] freqs freqCounts {}]  
+    (let [appliedCurrFreqCount (inc (get freqCounts currFreq 0))]
+      (if (= 2 appliedCurrFreqCount) 
+        currFreq
+        ;TODO Simplify this guy
+        (if (nil? restNewFreqs) 
+          (recur (+ currFreq currNewFreq) freqs (into freqCounts [{currFreq appliedCurrFreqCount}]))
+          (recur (+ currFreq currNewFreq) restNewFreqs (into freqCounts [{currFreq appliedCurrFreqCount}])))))))
+
+(def inputFile "01_input.txt")
+
+; Answer to part A
+(println (sum (map toInt (getLines inputFile))))
+
+;Answer to part B
+(println (firstFreqTwice (map toInt (getLines inputFile))))
